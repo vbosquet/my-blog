9598cad052e38eb8f18a9eb21b37e357ca67735b
T00VBOSQ <virginie.BOSQUET@fgtb.be>
2026-01-08 15:06:07 +0100

    debugging

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://my-blog-alpha-dun.vercel.app',
 	integrations: [mdx(), sitemap()],
 });
